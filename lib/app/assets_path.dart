ce106575a84caa0b1f4bb5a400d23f7bed6d435c
Imtiaz Shamim Rony <isr2020ss@gmail.com>
2025-04-09 20:28:28 +0600

    home_scree_update

--- lib/app/assets_path.dart
+++ lib/app/assets_path.dart
@@ -1,5 +1,5 @@
 class AssetsPath{
   static const String _imagePath= "assets/images";
   static const String appLogoSvg="$_imagePath/logo.svg";
-  static const String appLogoNavSvg="$_imagePath/logo_nav";
+  static const String appLogoNavSvg="$_imagePath/logo_nav.svg";
 }
--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'package:e_commerce_app_07/app/assets_path.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../widgets/app_bar_icon_button.dart';
+import '../widgets/home_carousel_slider.dart';
+import '../widgets/product_search_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,8 +14,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _searchBarController= TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height:16 ,),
+            ProductSearchBar(controller:_searchBarController ,) ,
+            SizedBox(height:16 ,),
+            HomeCarouselSlider()
+          ]
+      ) ,
+
+      ) ,
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: SvgPicture.asset(AssetsPath.appLogoNavSvg),
+      actions:[
+       AppBarIconButton(icon:Icons.person_outline, onTap: () { },),
+       AppBarIconButton(icon:Icons.call, onTap: () { },),
+       AppBarIconButton(icon:Icons.notifications, onTap: () { },),
+      ],
+    );
   }
 }
+
+
+
+
+
+
